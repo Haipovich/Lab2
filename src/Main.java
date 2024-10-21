06ab2de8482ed66a41957d4509690dd08ed30c34
NTHrd <hismatovegor@gmail.com>
2024-10-22 01:16:54 +0500

    init

---
+++ src/Main.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        while (true) {
+            System.out.println("Enter the number of task(1-6):");
+            switch (scan.nextInt()) {
+                case 1:
+                    Name nm1 = new Name("Клеопатра");
+                    Name nm2 = new Name("Пушкин", "Александр", "Сергеевич");
+                    Name nm3 = new Name("Маяковский", "Владимир");
+                    System.out.println(nm1);
+                    System.out.println(nm2);
+                    System.out.println(nm3);
+                    break;
+                case 2:
+                    House home = new House(scan.nextInt());
+                    System.out.println(home);
+                    break;
+                case 3:
+                    Department departmentIT = new Department("IT");
+                    Employee kozlov = new Employee("Kozlov", departmentIT);
+                    Employee sidorov = new Employee("Sidorov", departmentIT);
+                    Employee petrov = new Employee("Petrov", departmentIT);
+                    departmentIT.setHead(kozlov);
+                    System.out.println(sidorov);
+                    System.out.println(kozlov);
+                    System.out.println(petrov);
+                    break;
+                case 4:
+                    departmentIT = new Department("IT");
+                    kozlov = new Employee("Kozlov", departmentIT);
+                    sidorov = new Employee("Sidorov", departmentIT);
+                    petrov = new Employee("Petrov", departmentIT);
+                    departmentIT.setHead(kozlov);
+                    System.out.println(kozlov.get_department().get_employees());
+                    break;
+                case 5:
+                    Name_new nmw1 = new Name_new("Клеопатра");
+                    Name_new nmw2 = new Name_new("Александр", "Сергеевич", "Пушкин");
+                    Name_new nmw3 = new Name_new("Владимир", "Маяковский");
+                    Name_new nmw4 = new Name_new("Христофор", "Бонифатьевич");
+                    System.out.println(nmw1);
+                    System.out.println(nmw2);
+                    System.out.println(nmw3);
+                    System.out.println(nmw4);
+                    break;
+                case 6:
+                    Fraction f1 = new Fraction(1, 3);
+                    Fraction f2 = new Fraction(2, 5);
+                    Fraction f3 = new Fraction(1, 6);
+                    System.out.println(f1 + " + " + f2 + " = " + f1.sum(f2));
+                    System.out.println(f1 + " - " + f2 + " = " + f1.minus(f2));
+                    System.out.println(f1 + " * " + f3 + " = " + f1.multiply(f3));
+                    System.out.println(f3 + " / " + f2 + " = " + f3.divide(f2));
+                    System.out.println(f1.sum(f2).divide(f3).minus(5));
+                    break;
+                default:
+                    throw new IllegalStateException("Unexpected value");
+            }
+        }
+    }
+}
---
+++ src/Name_new.java
@@ -0,0 +1,34 @@
+public class Name_new {
+    private String last_name;
+    private String first_name;
+    private String middle_name;
+
+    public Name_new(String first_name, String last_name, String middle_name) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.middle_name = middle_name;
+    }
+
+    public Name_new(String first_name, String last_name) {
+        this(first_name, last_name, null);
+    }
+
+    public Name_new(String first_name) {
+        this(first_name, null, null);
+    }
+
+    @Override
+    public String toString() {
+        String result = "";
+        if (last_name != null) {
+            result += last_name + " ";
+        }
+        if (first_name != null) {
+            result += first_name + " ";
+        }
+        if (middle_name != null) {
+            result += middle_name;
+        }
+        return result.trim();
+    }
+}
